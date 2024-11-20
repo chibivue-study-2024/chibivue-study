a8d83d0721d4d8be1c3e91fa89267b02d639b6db
hashibadaiki <higemeganeking@gmail.com>
2024-11-20 12:18:33 +0900

    add: h関数の中身をHTMLに描画できるところまで

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,7 +1,14 @@
-export interface RendererOptions<HostNode = RendererNode> {
-  setElementText(node: HostNode, text: string): void
-}
+import { VNode } from './vnode'
 
+export interface RendererOptions<HostNode = RendererNode> {
+  createElement(type: string): HostNode // 追加
+
+  createText(text: string): HostNode // 追加
+
+  setElementText(node: HostNode, text: string): void
+
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void // 追加
+}
 export interface RendererNode {
   [key: string]: any
 }
@@ -14,10 +21,27 @@ export type RootRenderFunction<HostElement = RendererElement> = (
 ) => void
 
 export function createRenderer(options: RendererOptions) {
-  const { setElementText: hostSetElementText } = options
+  const {
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    insert: hostInsert,
+  } = options
 
-  const render: RootRenderFunction = (message, container) => {
-    hostSetElementText(container, message) // 今回はメッセージを挿入するだけなのでこういう実装になっている
+  function renderVNode(vnode: VNode | string) {
+    if (typeof vnode === 'string') return hostCreateText(vnode)
+    const el = hostCreateElement(vnode.type)
+
+    for (const child of vnode.children) {
+      const childEl = renderVNode(child)
+      hostInsert(childEl, el)
+    }
+
+    return el
+  }
+
+  const render: RootRenderFunction = (vnode, container) => {
+    const el = renderVNode(vnode)
+    hostInsert(el, container)
   }
 
   return { render }
--- packages/runtime-dom/nodeOps.ts
+++ packages/runtime-dom/nodeOps.ts
@@ -1,7 +1,22 @@
 import { RendererOptions } from '../runtime-core'
 
 export const nodeOps: RendererOptions<Node> = {
+  // 追加
+  createElement: tagName => {
+    return document.createElement(tagName)
+  },
+
+  // 追加
+  createText: (text: string) => {
+    return document.createTextNode(text)
+  },
+
   setElementText(node, text) {
     node.textContent = text
+  },
+
+  // 追加
+  insert: (child, parent, anchor) => {
+    parent.insertBefore(child, anchor || null)
   },
 }
